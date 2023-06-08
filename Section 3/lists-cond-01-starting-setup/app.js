42626dd54112f824b9bb0aadc19cf6b0b1191367
Anisa Majmari <anisamajmari@gmail.com>
2023-06-08 09:56:32 +0200

    removing list items

--- Section 3/lists-cond-01-starting-setup/app.js
+++ Section 3/lists-cond-01-starting-setup/app.js
@@ -7,6 +7,9 @@ const app = Vue.createApp({
       this.goals.push(this.enteredGoal);
       this.enteredGoal = "";
     },
+    removeGoal(index) {
+      this.goals.splice(index, 1);
+    },
   },
 });
 
