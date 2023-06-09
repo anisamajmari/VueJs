e5ab7fcb2df5be087d0fe7f6803311e0c64799a1
Anisa Majmari <anisamajmari@gmail.com>
2023-06-09 15:19:46 +0200

    adding a battle log

--- Section 4/prj-monster-01-starting-setup/app.js
+++ Section 4/prj-monster-01-starting-setup/app.js
@@ -9,6 +9,7 @@ const app = Vue.createApp({
       monsterHealth: 100,
       currentRound: 0,
       winner: null,
+      logMessages: [],
     };
   },
   methods: {
@@ -17,23 +18,27 @@ const app = Vue.createApp({
       this.monsterHealth = 100;
       this.currentRound = 0;
       this.winner = null;
+      this.logMessages = [];
     },
     attackMonster() {
       this.currentRound++;
       const attackValue = getValue(5, 12);
       this.monsterHealth -= attackValue;
+      this.addLogMessage("player", "attack", attackValue);
       this.attackPlayer();
     },
 
     attackPlayer() {
       const attackValue = getValue(8, 15);
       this.playerHealth -= attackValue;
+      this.addLogMessage("monster", "attack", attackValue);
     },
 
     specialAttackMonster() {
       this.currentRound++;
       const attackValue = getValue(10, 25);
       this.monsterHealth -= attackValue;
+      this.addLogMessage("player", "attack", attackValue);
       this.attackPlayer();
     },
 
@@ -45,11 +50,20 @@ const app = Vue.createApp({
       } else {
         this.playerHealth += healValue;
       }
+      this.addLogMessage("player", "heal", healValue);
       this.attackPlayer();
     },
 
     surrender() {
       this.winner = "monster";
+    },
+
+    addLogMessage(who, what, value) {
+      this.logMessages.unshift({
+        actionBy: who,
+        actionType: what,
+        actionValue: value,
+      });
     },
   },
   computed: {
