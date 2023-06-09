20cb590539fba3d1650b05fe4e475c9bf3294ada
Anisa Majmari <anisamajmari@gmail.com>
2023-06-09 14:41:05 +0200

    game over

--- Section 4/prj-monster-01-starting-setup/app.js
+++ Section 4/prj-monster-01-starting-setup/app.js
@@ -7,10 +7,13 @@ const app = Vue.createApp({
     return {
       playerHealth: 100,
       monsterHealth: 100,
+      currentRound: 0,
+      winner: null,
     };
   },
   methods: {
     attackMonster() {
+      this.currentRound++;
       const attackValue = getValue(5, 12);
       this.monsterHealth -= attackValue;
       this.attackPlayer();
@@ -20,13 +23,55 @@ const app = Vue.createApp({
       const attackValue = getValue(8, 15);
       this.playerHealth -= attackValue;
     },
+
+    specialAttackMonster() {
+      this.currentRound++;
+      const attackValue = getValue(10, 25);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
+    },
+
+    healPlayer() {
+      this.currentRound++;
+      const healValue = getValue(8, 20);
+      if (this.playerHealth + healValue > 100) {
+        this.playerHealth = 100;
+      } else {
+        this.playerHealth += healValue;
+      }
+      this.attackPlayer();
+    },
   },
   computed: {
     monsterBarStyles() {
       return { width: this.monsterHealth + "%" };
     },
+
     playerBarStyles() {
       return { width: this.playerHealth + "%" };
+    },
+
+    mayUseSpecialAttack() {
+      return this.currentRound % 3 !== 0;
+    },
+  },
+
+  watch: {
+    playerHealth(value) {
+      if (value <= 0 && this.monsterHealth <= 0) {
+        this.winner = "draw";
+      } else if (value <= 0) {
+        // player lost
+        this.winner = "monster";
+      }
+    },
+    monsterHealth(value) {
+      if (value <= 0 && this.playerHealth <= 0) {
+        this.winner = "draw";
+      } else if (value <= 0) {
+        // monster lost
+        this.winner = "player";
+      }
     },
   },
 });
