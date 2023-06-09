1b4ae28e30a4102781b03e2c6d4432a6ebaf64d6
Anisa Majmari <anisamajmari@gmail.com>
2023-06-09 17:01:54 +0200

    understanding templates

--- Section 5/behind-scenes-01-starting-setup/app.js
+++ Section 5/behind-scenes-01-starting-setup/app.js
@@ -1,8 +1,8 @@
 const app = Vue.createApp({
   data() {
     return {
-      currentUserInput: '',
-      message: 'Vue is great!',
+      currentUserInput: "",
+      message: "Vue is great!",
     };
   },
   methods: {
@@ -15,4 +15,40 @@ const app = Vue.createApp({
   },
 });
 
-app.mount('#app');
+app.mount("#app");
+
+///////////
+
+const app2 = Vue.createApp({
+  template: `
+  <p>{{ favoriteMeal }}</p>
+  `,
+  data() {
+    return {
+      favoriteMeal: "Pizza",
+    };
+  },
+});
+app2.mount("#app2");
+
+///////////////////
+
+// const data = {
+//   message: "Hello",
+//   longMessage: "Helloo! world",
+// };
+
+// const handler = {
+//   set(target, key, value) {
+//     if (key === "message") {
+//       target.longMessage = value + "World";
+//     }
+//     target.message = value;
+//   },
+// };
+
+// const proxy = new Proxy(data, handler);
+
+// proxy.message = "Helloo!!!!";
+
+// console.log(proxy.longMessage);
