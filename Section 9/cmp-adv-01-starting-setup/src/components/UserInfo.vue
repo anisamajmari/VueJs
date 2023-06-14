ddec9017fde5d74776e21180ffce1e652e7b1217
Anisa Majmari <anisamajmari@gmail.com>
2023-06-14 12:19:17 +0200

    introducing slots

--- Section 9/cmp-adv-01-starting-setup/src/components/UserInfo.vue
+++ Section 9/cmp-adv-01-starting-setup/src/components/UserInfo.vue
@@ -1,10 +1,14 @@
 <template>
   <section>
-    <header>
-      <h3>{{ fullName }}</h3>
-      <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
-    </header>
-    <p>{{ infoText }}</p>
+    <base-card>
+      <template v-slot:header>
+        <h3>{{ fullName }}</h3>
+        <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
+      </template>
+      <template v-slot:default>
+        <p>{{ infoText }}</p>
+      </template>
+    </base-card>
   </section>
 </template>
 
@@ -16,13 +20,6 @@ export default {
 
 <style scoped>
 /* scoped => tells vue that the styles that defined here should only affect the template in the same file */
-section {
-  margin: 2rem auto;
-  max-width: 30rem;
-  border-radius: 12px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  padding: 1rem;
-}
 
 section header {
   display: flex;
