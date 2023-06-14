f1888164e9a08dd42a9b50b01e0c0aec56d54739
Anisa Majmari <anisamajmari@gmail.com>
2023-06-14 13:08:16 +0200

    dynamic component

--- Section 9/cmp-adv-01-starting-setup/src/App.vue
+++ Section 9/cmp-adv-01-starting-setup/src/App.vue
@@ -2,7 +2,7 @@
   <div>
     <!-- <TheHeader /> -->
     <the-header></the-header>
-    <badge-list></badge-list>
+    <!-- <badge-list></badge-list>
     <user-info
       :full-name="activeUser.name"
       :info-text="activeUser.description"
@@ -10,37 +10,56 @@
     ></user-info>
     <course-goals>
       <template #default="slotProps">
-        <!-- scoped slots -->
+        scoped slots
         <h2>{{ slotProps.item }}</h2>
         <p>{{ slotProps["another-prop"] }}</p>
       </template>
-    </course-goals>
+    </course-goals> -->
+    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+
+    <!-- <active-goals v-if="selectedComponent === 'active-goals'"></active-goals>
+    <manage-goals v-if="selectedComponent === 'manage-goals'"></manage-goals> -->
+    <!-- DYNAMIC COMPONENT -->
+    <keep-alive>
+      <component :is="selectedComponent"></component>
+    </keep-alive>
   </div>
 </template>
 
 <script>
 import TheHeader from "./components/TheHeader.vue";
-import BadgeList from "./components/BadgeList.vue";
-import UserInfo from "./components/UserInfo.vue";
-import CourseGoals from "./components/CourseGoals.vue";
+// import BadgeList from "./components/BadgeList.vue";
+// import UserInfo from "./components/UserInfo.vue";
+// import CourseGoals from "./components/CourseGoals.vue";
+import ActiveGoals from "./components/ActiveGoals.vue";
+import ManageGoals from "./components/ManageGoals.vue";
 
 export default {
   components: {
     // "the-header": TheHeader,
     // TheHeader: TheHeader,
     TheHeader,
-    BadgeList,
-    UserInfo,
-    CourseGoals,
+    // BadgeList,
+    // UserInfo,
+    // CourseGoals,
+    ManageGoals,
+    ActiveGoals,
   },
   data() {
     return {
+      selectedComponent: "active-goals",
       activeUser: {
         name: "Maximilian Schwarzmüller",
         description: "Site owner and admin",
         role: "admin",
       },
     };
+  },
+  methods: {
+    setSelectedComponent(component) {
+      this.selectedComponent = component;
+    },
   },
 };
 </script>
