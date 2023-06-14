099b3050b29dcf20d365c424d57b1e73b1954500
Anisa Majmari <anisamajmari@gmail.com>
2023-06-14 14:26:26 +0200

    challenge

---
+++ Challenges/cmp-communication-assignment-problem/src/components/UserData.vue
@@ -0,0 +1,34 @@
+<template>
+  <section>
+    <form>
+      <div>
+        <label>Name</label>
+        <input type="text" v-model="inputName" />
+      </div>
+
+      <div>
+        <label>Age</label>
+        <input type="text" v-model="inputAge" />
+      </div>
+      <button @submit.prevent="submitData">Submit</button>
+    </form>
+  </section>
+</template>
+
+<script>
+export default {
+  emits: ["set-data"],
+
+  data() {
+    return {
+      inputName: "",
+      inputAge: "",
+    };
+  },
+  methods: {
+    submitData() {
+      this.$emit("set-data", this.inputAge, this.inputName);
+    },
+  },
+};
+</script>
--- Challenges/cmp-communication-assignment-problem/src/main.js
+++ Challenges/cmp-communication-assignment-problem/src/main.js
@@ -1,9 +1,7 @@
-import { createApp } from 'vue'
+import { createApp } from "vue";
 
-createApp({}).mount('#app')
-
-// Task 1: 
-// Add two components to the app: 
+// Task 1:
+// Add two components to the app:
 // An ActiveUser component and an UserData component
 // ActiveUser should output a username (h2) and age (h3)
 // UserData should output two input fields => for name and age
@@ -14,3 +12,14 @@ createApp({}).mount('#app')
 // Task 3: Add user data and ensure it contains a name and age
 // User data should be output in ActiveUser
 // It should be updated via the UserData component
+
+import App from "./App.vue";
+import ActiveUser from "./components/ActiveUser.vue";
+import UserData from "./components/UserData.vue";
+
+const app = createApp(App);
+
+app.component("active-user", ActiveUser);
+app.component("user-data", UserData);
+
+app.mount("#app");
