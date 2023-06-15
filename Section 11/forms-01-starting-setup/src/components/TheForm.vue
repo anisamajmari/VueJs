25efd98da286fdf07d58e018fea9922791c9c4c1
Anisa Majmari <anisamajmari@gmail.com>
2023-06-15 17:13:28 +0200

    v-model and dropdowns

--- Section 11/forms-01-starting-setup/src/components/TheForm.vue
+++ Section 11/forms-01-starting-setup/src/components/TheForm.vue
@@ -1,16 +1,16 @@
 <template>
-  <form>
+  <form @submit.prevent="submitForm">
     <div class="form-control">
       <label for="user-name">Your Name</label>
-      <input id="user-name" name="user-name" type="text" />
+      <input id="user-name" name="user-name" type="text" v-model="nameInput" />
     </div>
     <div class="form-control">
       <label for="age">Your Age (Years)</label>
-      <input id="age" name="age" type="number" />
+      <input id="age" name="age" type="number" v-model="ageInput" />
     </div>
     <div class="form-control">
       <label for="referrer">How did you hear about us?</label>
-      <select id="referrer" name="referrer">
+      <select id="referrer" name="referrer" v-model="referrer">
         <option value="google">Google</option>
         <option value="wom">Word of mouth</option>
         <option value="newspaper">Newspaper</option>
@@ -51,6 +51,28 @@
     </div>
   </form>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      nameInput: '',
+      ageInput: null,
+      referrer: 'wom',
+    };
+  },
+  methods: {
+    submitForm() {
+      console.log('Name:  ' + this.nameInput);
+      this.nameInput = '';
+      console.log('Age:  ' + this.ageInput);
+      this.ageInput = '';
+      console.log('Referrer: ' + this.referrer);
+      this.referrer = 'wom';
+    },
+  },
+};
+</script>
 
 <style scoped>
 form {
