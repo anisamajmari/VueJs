586815ceb975297e75076adcbc11319d2569b2ca
Anisa Majmari <anisamajmari@gmail.com>
2023-06-21 18:53:21 +0200

    wiring up pages

--- Section 16/main-prj-01-starting-setup/src/router.js
+++ Section 16/main-prj-01-starting-setup/src/router.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-import CoachDetails from './pages/coaches/CoachDetails.vue';
+import CoachDetails from './pages/coaches/CoachDetail.vue';
 import CoachesList from './pages/coaches/CoachesList.vue';
 import CoachRegistration from './pages/coaches/CoachRegistration.vue';
 import ContactCoach from './pages/requests/ContactCoach.vue';
