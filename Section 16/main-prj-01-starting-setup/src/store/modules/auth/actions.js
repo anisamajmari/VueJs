0d189b8eacf439591d949e5ff33b2b3d5b7323c5
Anisa Majmari <anisamajmari@gmail.com>
2023-06-23 17:38:25 +0200

    adding a login action & flow

--- Section 16/main-prj-01-starting-setup/src/store/modules/auth/actions.js
+++ Section 16/main-prj-01-starting-setup/src/store/modules/auth/actions.js
@@ -1,17 +1,42 @@
+import { API_KEY, API_SIGNIN_URL, API_SIGNUP_URL } from '../../../config.js';
+
 export default {
-  login() {},
+  async login(context, payload) {
+    const response = await fetch(`${API_SIGNIN_URL}key=${API_KEY}`, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: payload.email,
+        password: payload.password,
+        returnSecureToken: true,
+      }),
+    });
+
+    const responseData = await response.json();
+    if (!response.ok) {
+      console.log(responseData);
+      const error = new Error(
+        responseData.message || 'Failed to authenticate!'
+      );
+      throw error;
+    }
+
+    console.log(responseData);
+    context.commit('setUser', {
+      token: responseData.idToken,
+      userId: responseData.localId,
+      tokenExpiration: responseData.expiresIn,
+    });
+  },
+
   async signup(context, payload) {
-    const response = await fetch(
-      `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB1Gx7R7WEnKjfUjCC_sCVADPOphSDlW5M`,
-      {
-        method: 'POST',
-        body: JSON.stringify({
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true,
-        }),
-      }
-    );
+    const response = await fetch(`${API_SIGNUP_URL}key=${API_KEY}`, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: payload.email,
+        password: payload.password,
+        returnSecureToken: true,
+      }),
+    });
 
     const responseData = await response.json();
     if (!response.ok) {
