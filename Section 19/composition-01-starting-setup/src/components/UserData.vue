9581bd9835ef9a717f61a97e737ecf88632b9563
Anisa Majmari <anisamajmari@gmail.com>
2023-07-07 19:49:40 +0200

    components, props and composition API

---
+++ Section 19/composition-01-starting-setup/src/components/UserData.vue
@@ -0,0 +1,25 @@
+<template>
+  <h2>{{ userName }}</h2>
+  <h3>{{ age }}</h3>
+</template>
+
+<script>
+import { computed } from 'vue';
+export default {
+  props: ['firstName', 'lastName', 'age'],
+  //   computed: {
+  //     userName() {
+  //       return this.firstName + ' ' + this.lastName;
+  //     },
+  //   },
+  setup(props) {
+    const uName = computed(function () {
+      return props.firstName + ' ' + props.lastName;
+    });
+
+    return {
+      userName: uName,
+    };
+  },
+};
+</script>
