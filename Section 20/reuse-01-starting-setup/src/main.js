d88078c46392fec1cfcbc4217d818694a0769b7e
Anisa Majmari <anisamajmari@gmail.com>
2023-07-10 19:29:40 +0200

    Global mixins

--- Section 20/reuse-01-starting-setup/src/main.js
+++ Section 20/reuse-01-starting-setup/src/main.js
@@ -1,7 +1,11 @@
 import { createApp } from 'vue';
 
 import App from './App.vue';
+import loggerMixing from './mixins/logger';
 
-const app = createApp(App)
+const app = createApp(App);
+
+// Global mixins
+app.mixin(loggerMixing);
 
 app.mount('#app');
