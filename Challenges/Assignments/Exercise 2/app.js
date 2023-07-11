b3cf6700789961f40e1774c9b3d85c11fafacb64
Anisa Majmari <anisamajmari@gmail.com>
2023-07-11 10:17:24 +0200

    exercise 2 completed

--- Challenges/Assignments/Exercise 2/app.js
+++ Challenges/Assignments/Exercise 2/app.js
@@ -1,6 +1,19 @@
-new Vue({
-        el: '#exercise',
-        data: {
-            value: ''
-        }
-    });
+const app = Vue.createApp({
+  data() {
+    return {
+      value: "",
+    };
+  },
+
+  methods: {
+    buttonClicked() {
+      alert("Button is clicked!!!");
+    },
+
+    updateValue(event) {
+      this.value = event.target.value;
+    },
+  },
+});
+
+app.mount("#exercise");
