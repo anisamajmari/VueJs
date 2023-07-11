8b34b28f15d51d903e2a3091aac53e48ed4a492a
Anisa Majmari <anisamajmari@gmail.com>
2023-07-11 10:36:00 +0200

    exercise 3 completed

--- Challenges/Assignments/Exercise 3/app.js
+++ Challenges/Assignments/Exercise 3/app.js
@@ -1,6 +1,27 @@
-new Vue({
-        el: '#exercise',
-        data: {
-            value: 0
-        }
-    });
+const app = Vue.createApp({
+  data() {
+    return {
+      value: 0,
+    };
+  },
+
+  computed: {
+    result() {
+      if (this.value === 37) {
+        return "Done.";
+      } else {
+        return "Not there yet!";
+      }
+    },
+  },
+
+  watch: {
+    result() {
+      setTimeout(() => {
+        this.value = 0;
+      }, 5000);
+    },
+  },
+});
+
+app.mount("#exercise");
